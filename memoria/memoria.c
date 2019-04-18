b15e9af350a7b003eacfb7dc7d83f4b048e3d815
utnso <utnso@ubuntu-server>
2019-04-18 11:49:17 -0300

    sockets

--- memoria/memoria.c
+++ memoria/memoria.c
@@ -1,1 +1,21 @@
-/*Memoria*/
+#include <stdio.h>
+#include <funcionesCompartidas/log.h>
+#include <funcionesCompartidas/funcionesNET.h>
+
+int main(){
+    t_log *file_log = crear_archivo_log("Kernel", true,"/home/utnso/git/tp-2019-1c-misc/memoria/log");
+    log_info(file_log, "cargando el archivo de configuracion");
+    int control = 0;
+    log_info(file_log,"estableciendo conexion");
+    int socketClient = establecerConexion("10.148.106.248","4020",file_log,&control);
+    header headSend;
+    headSend.letra = 'D';
+    headSend.codigo = 1;
+    headSend.sizeData = 13;
+    void * bloqueData = "hola Como va";
+    message *bufferRes = createMessage(&headSend, bloqueData);
+    if (enviar_message(socketClient, bufferRes, file_log, &control) < 0) {
+        log_info(file_log, "Error al enviar el bloque");
+        return -1;
+    }
+}
