543178b928695f400fa9a4f329589a7f8cad0b63
Miguel Chauca <miguel.chauca@almundo.com>
2019-04-18 22:59:36 -0300

    agregando ejemplo de socket con distintos tipos de mensajes

--- example/client.c
+++ example/client.c
@@ -1,20 +1,168 @@
 #include <stdio.h>
 #include <funcionesCompartidas/log.h>
 #include <funcionesCompartidas/funcionesNET.h>
+#include <readline/readline.h>
+
+
+void enviarMjsString(int socket, t_log * log){
+    // creamos el header
+    char * bloqueData = "hola Como va";
+    int control = 0;
+    header headSend;
+    //para identificar de que componente viene
+    headSend.letra = 'M';
+    //para identificar el tipo de mensaje
+    headSend.codigo = 1;
+    //tamaño de la data que se va enviar en este caso es un string usamos strlen
+    //y le sumamos 1 por que hay q agregarle el \0 el fin de cadena
+    headSend.sizeData = strlen(bloqueData) + 1;
+    //creamos el mjs y nos devuelve el paquetes de datos a enviar pasamos la direccion del header y el bloque de data a enviar
+    message *bufferRes = createMessage(&headSend, bloqueData);
+    //enviamos el mjs se pasa el socket , la direccion del log para logear error interno y un direccion de int para tomar accion
+    //sobre los resultados
+    if (enviar_message(socket, bufferRes, log, &control) < 0) {
+        log_info(log, "Error al enviar el bloque");
+    }
+    log_info(log, "Se envio el mjs valla a mirar la respuesta en el server");
+}
+
+typedef struct{
+    int key;
+    char letra;
+    int listNumero[5];
+} __attribute__((packed)) structSinPuntero;
+
+void enviarMjsStructSinPuntero(int socket, t_log * log){
+    structSinPuntero bloqueData;
+    bloqueData.key = 4;
+    bloqueData.letra = 'M';
+    //limpiamos el array cargamos todo con cero
+    memset(bloqueData.listNumero,0, sizeof(bloqueData.listNumero));
+    //cargamos un par de posiciones
+    bloqueData.listNumero[0] = 12;
+    bloqueData.listNumero[1] = 39;
+    bloqueData.listNumero[2] = 33;
+    int control = 0;
+    header headSend;
+    //para identificar de que componente viene
+    headSend.letra = 'M';
+    //para identificar el tipo de mensaje
+    headSend.codigo = 2;
+    //tamaño de la data que se va enviar en este caso sizeof nos devuelve el tamaño de la structura
+    headSend.sizeData = sizeof(bloqueData);
+    //creamos el mjs y nos devuelve el paquetes de datos a enviar pasamos la direccion del header y el bloque de data a enviar
+    message *bufferRes = createMessage(&headSend, &bloqueData);
+    //enviamos el mjs se pasa el socket , la direccion del log para logear error interno y un direccion de int para tomar accion
+    //sobre los resultados
+    if (enviar_message(socket, bufferRes, log, &control) < 0) {
+        log_info(log, "Error al enviar el bloque");
+    }
+    log_info(log, "Se envio el mjs valla a mirar la respuesta en el server");
+}
+
+typedef struct {
+    char * nombre;
+    char * apellido;
+    char * telefono;
+    int edad;
+}structDatosPersonales;
+
+typedef struct {
+    size_t sizeNombre;
+    size_t sizeApellido;
+    size_t sizeTelefono;
+    size_t sizeEdad;
+} __attribute__((packed)) structDataPersonalesSize;
+
+void crearMjsStructConPuntero(int socket, t_log * log){
+    structDatosPersonales dataPersonal;
+    //creamos esta structura para guardar los tamaños de los datos de la structura
+    structDataPersonalesSize dataPersonalSize;
+    dataPersonal.nombre = "Miguel Angel";
+    dataPersonal.apellido = "Chauca Sanchez";
+    dataPersonal.telefono = "11-2308-1763";
+    dataPersonal.edad = 29;
+    //guardamos todos los tamaños de cada propiedad de la structura
+    dataPersonalSize.sizeNombre = strlen(dataPersonal.nombre)+ 1;
+    dataPersonalSize.sizeApellido = strlen(dataPersonal.apellido)+ 1;
+    dataPersonalSize.sizeTelefono = strlen(dataPersonal.telefono) +1;
+    dataPersonalSize.sizeEdad = sizeof(int);
+    //sumamos el tamaño de cada propiedad mas el tamaño de la structura que guarda los tamaños
+    size_t sizeBufferAEnviar = sizeof(dataPersonalSize) + dataPersonalSize.sizeNombre + dataPersonalSize.sizeApellido + dataPersonalSize.sizeTelefono + dataPersonalSize.sizeEdad;
+    //creamos un buffer con el tamaño de dato que vamos a enviar
+    void * bufferAEnviar = malloc(sizeBufferAEnviar);
+    //offset para posicionar el puntero al final de lo copiado en el buffer
+    int offset = 0;
+    //copiamos al buffer la structura con los size
+    memcpy((bufferAEnviar + offset),&dataPersonalSize, sizeof(dataPersonalSize));
+    //sumamos lo copiado
+    offset += sizeof(dataPersonalSize);
+    //copiamos las propiedades que se va enviar
+    memcpy((bufferAEnviar + offset),dataPersonal.nombre, dataPersonalSize.sizeNombre);
+    offset += dataPersonalSize.sizeNombre;
+
+    memcpy((bufferAEnviar + offset),dataPersonal.apellido,dataPersonalSize.sizeApellido);
+    offset += dataPersonalSize.sizeApellido;
+
+    memcpy((bufferAEnviar + offset),dataPersonal.telefono,dataPersonalSize.sizeTelefono);
+    offset += dataPersonalSize.sizeTelefono;
+
+    memcpy((bufferAEnviar+ offset),&dataPersonal.edad,dataPersonalSize.sizeEdad);
+
+    //ahora creamos el mensaje a enviar ya que tenemos todo el buffer cargado
+    //cargamos el header
+    header request;
+    request.letra = 'M';
+    request.codigo = 3;
+    request.sizeData = sizeBufferAEnviar;
+
+    int control= 0;
+    void * bufferDeMensaje = createMessage(&request,bufferAEnviar);
+
+    if (enviar_message(socket, bufferDeMensaje, log, &control) < 0) {
+        log_info(log, "Error al enviar el bloque");
+    }
+    log_info(log, "Se envio el mjs valla a mirar la respuesta en el server");
+}
+
 int main(){
-    t_log *file_log = crear_archivo_log("Kernel", true,"/home/logC");
-    log_info(file_log, "cargando el archivo de configuracion");
+    t_log *file_log = crear_archivo_log("Kernel", true,"./logC");
     int control = 0;
     log_info(file_log,"estableciendo conexion");
     int socketClient = establecerConexion("127.0.0.1","4020",file_log,&control);
-    header headSend;
-    headSend.letra = 'D';
-    headSend.codigo = 1;
-    headSend.sizeData = 13;
-    void * bloqueData = "hola Como va";
-    message *bufferRes = createMessage(&headSend, bloqueData);
-    if (enviar_message(socketClient, bufferRes, file_log, &control) < 0) {
-        log_info(file_log, "Error al enviar el bloque");
+    if (control != 0) {
+        log_error(file_log, "Error al intentar establecer conneccion");
         return -1;
+    }
+    printf("option 1 para un mensaje con string\n");
+    printf("option 2 para un mensaje con structura sin punteros\n");
+    printf("option 3 para un mensaje con structura con punteros\n");
+    printf("option -1 para salir\n");
+    char * option;
+    int optinNumber;
+    option = readline(">");
+    while (strcmp(option,"-1") != 0){
+        optinNumber = strtol(option, NULL, 10);
+        switch (optinNumber){
+            case 1:{
+                log_info(file_log,"enviando solo un string");
+                enviarMjsString(socketClient,file_log);
+                break;
+            }
+            case 2: {
+                log_info(file_log,"enviando con structura sin puntero");
+                enviarMjsStructSinPuntero(socketClient,file_log);
+                break;
+            }
+            case 3:{
+                log_info(file_log,"enviando con structura con puntero");
+                crearMjsStructConPuntero(socketClient,file_log);
+                break;
+            }
+            default: {
+                printf("no se reconoce ese mjs\n");
+            }
+        }
+        option = readline(">");
     }
 }
--- example/server.c
+++ example/server.c
@@ -1,13 +1,107 @@
 #include <funcionesCompartidas/funcionesNET.h>
 
-void main(){
-    t_log * log = crear_archivo_log("Server",true,"/home/logS");
-    log_info(log,"Hola hice lago");
+typedef struct{
+    int key;
+    char letra;
+    int listNumero[5];
+} structSinPuntero;
+
+typedef struct {
+    char * nombre;
+    char * apellido;
+    char * telefono;
+    int edad;
+}structDatosPersonales;
+
+typedef struct {
+    size_t sizeNombre;
+    size_t sizeApellido;
+    size_t sizeTelefono;
+    size_t sizeEdad;
+} __attribute__((packed)) structDataPersonalesSize;
+
+void armarStructuraConPuntero(void * buffer){
+    structDataPersonalesSize dataPersonalSize;
+    structDatosPersonales dataPersonal;
+    int offset = 0;
+    //como copiamo primero la strucutra don la data size cargamos eso primero por que nos va ayudar a cargar los otros datos
+    memcpy(&dataPersonalSize,(buffer + offset), sizeof(structDataPersonalesSize));
+    //sumamos lo que ya se copio
+    offset += sizeof(structDataPersonalesSize);
+    //copiamos las propiedades en el orden que se copiaron el cliente
+    dataPersonal.nombre = malloc(dataPersonalSize.sizeNombre);
+    memcpy(dataPersonal.nombre,(buffer + offset), dataPersonalSize.sizeNombre);
+    offset += dataPersonalSize.sizeNombre;
+
+    dataPersonal.apellido = malloc(dataPersonalSize.sizeApellido);
+    memcpy(dataPersonal.apellido,(buffer + offset), dataPersonalSize.sizeApellido);
+    offset += dataPersonalSize.sizeApellido;
+
+    dataPersonal.telefono = malloc(dataPersonalSize.sizeTelefono);
+    memcpy(dataPersonal.telefono,(buffer + offset), dataPersonalSize.sizeTelefono);
+    offset += dataPersonalSize.sizeTelefono;
+
+    memcpy(&dataPersonal.edad,(buffer + offset), dataPersonalSize.sizeEdad);
+
+    printf("nombre = %s \n", dataPersonal.nombre);
+    printf("apellido = %s \n", dataPersonal.apellido);
+    printf("telefono = %s \n", dataPersonal.telefono);
+    printf("edad = %d \n", dataPersonal.edad);
+
+}
+
+int main(){
+    t_log * log = crear_archivo_log("Server",true,"./logS");
     int control = 0;
+    //instanciar un socket server
+    log_info(log,"creando server");
     int fdServer = makeListenSock("4020",log,&control);
+    //se queda a la espera de alquien que se conecte
+    log_info(log,"esperando a que se conecte alquien");
     int sockConect = aceptar_conexion(fdServer,log,&control);
     header request;
     void * buffer;
-    char * result = getMessage(sockConect, &request, &control);
-    printf("resultado --- %s", result);
+    while (1) {
+        //recibir el mensaje definimos un header para que se nos cargue el tipo de dato enviado
+        //y un buffer con para cargarlo con el dato
+        log_info(log,"esperando a que ese alquien me envie un mjs");
+         buffer = getMessage(sockConect, &request, &control);
+        //verificamos si nos llego el mjs si es NULL fallo;
+        if(buffer){
+            //verificamos tipo de mjs
+            switch(request.codigo){
+                case 1:{
+                    //si es caso 1 nos llega un string
+                    log_info(log, "entro mensaje con el codigo 1");
+                    printf("mensaje --> %s \n", (char *)buffer);
+                    break;
+                }
+                case 2:{
+                    log_info(log, "entro mensaje con el codigo 2");
+                    structSinPuntero dataAux;
+                    //al ser una structura sin punteros cargamos la dataAux de forma directa en una estrucura
+                    //le pasamos la direccion de la structura, el buffer q nos llego del getMesaage
+                    //y le pasamos el tamaño del buffer que nos llega en el header
+                    memcpy(&dataAux,buffer,request.sizeData);
+                    printf("key [%d], letra[%c] \n",dataAux.key,dataAux.letra);
+                    int i = 0;
+                    for (i = 0; i < 5; ++i) {
+                        printf("listNumero[%d] = %d \n",i,dataAux.listNumero[i]);
+                    }
+                    break;
+                }
+                case 3: {
+                    log_info(log, "entro mensaje con el codigo 3");
+                    armarStructuraConPuntero(buffer);
+                    break;
+                }
+                default:{
+                    printf("no entiendo el mjs\n");
+                }
+            }
+        }else{
+            log_error(log,"se desconecto el cliente");
+            return -1;
+        }
+    }
 }
