85936b6ecc9ce9469ceebeaa3c60114842a2851b
cruz636 <juanbarcruz96@gmail.com>
2019-06-07 19:57:53 -0300

    MAS VALE QUE TE ANDE

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -7,14 +7,55 @@
 #include <pthread.h>
 #include <funcionesCompartidas/API.h>
 
-#define IPFILESYSTEM "192.168.0.21"
+#define IPFILESYSTEM "192.168.0.21" //shall we save this info in the .config file???
 #define PORTFILESYSTEM "8080"
 #define DIM 1//total pages (?)
 
 t_log *log_server;
 t_config * g_config;
 
+char value; //tamanio máximo para configuración de las páginas
+char *IP_FS = IPFILESYSTEM;
+char *PORT_FS = PORTFILESYSTEM;
 
+
+
+///
+
+typedef struct {
+    enum OPERACION operacion;
+    char *nameTable;
+    int key;
+} st_select;
+
+typedef struct {
+    size_t operacion;
+    size_t nameTable;
+    size_t key;
+}__attribute__((packed)) st_size_select;
+
+st_select *deserealizarSelect(void *buffer) {
+    st_size_select sizeSelect;
+    st_select *_select = malloc(sizeof(st_select));
+    int offset = 0;
+
+    memcpy(&sizeSelect, buffer, sizeof(st_size_select));
+    offset += sizeof(st_size_select);
+
+    memcpy(&_select->operacion, (buffer + offset), sizeSelect.operacion);
+    offset += sizeSelect.operacion;
+
+    _select->nameTable = malloc(sizeSelect.nameTable);
+    memcpy(_select->nameTable, (buffer + offset), sizeSelect.nameTable);
+    offset += sizeSelect.nameTable;
+
+    memcpy(&_select->key, (buffer + offset), sizeSelect.key);
+
+    return _select;
+}
+
+
+////
 struct segmentTable{
 
 };
@@ -35,14 +76,10 @@ void leerMensaje(void *recibido,header request);
 void enviarMjsString(int socket, t_log * log);
 void enviarAFileSystem(enum OPERACION operacion,size_t sizeBuffer,void * buffer);
 
-void connectToFL(char *IP,char *PORT);
+void connectToFileSystem(char *IP,char *PORT);
 
 
 int main(int argc, char *argv[]) {
-
-    char *IP_FS = IPFILESYSTEM;
-    char *PORT_FS = PORTFILESYSTEM;
-
 
     for(int i=0;i < DIM ; i++){
         printf("[>] Name of page N°%i : ",i);
@@ -83,7 +120,8 @@ int main(int argc, char *argv[]) {
   //that was all the memoriaConf.config info ;)
 
 
-    connectToFL(IP_FS,PORT_FS);
+    connectToFileSystem(IP_FS,PORT_FS); //we have to connect to the FS to recive the handshake
+
 
 
 
@@ -102,14 +140,13 @@ int main(int argc, char *argv[]) {
 }
 
 
-void connectToFL(char *IP,char *PORT){
+void connectToFileSystem(char *IP,char *PORT){
 
-    t_log *file_log = crear_archivo_log("Kernel", true,"./logC");
+    t_log *file_log = crear_archivo_log("PrimaryMemory", true,"./logC");
     int control = 0;
-    log_info(file_log,"getting handshake..");
+    log_info(file_log,"getting handshake.."); //delete (?)
 
     printf("[+]Connecting to FILESYSTEM AT : %s:%s \n",IP,PORT);
-
 
     int socketClient = establecerConexion(IP,PORT,file_log,&control);
 
@@ -118,8 +155,9 @@ void connectToFL(char *IP,char *PORT){
         return -1;
     }
 
-    enviarMjsString(socketClient,file_log);
     printf("[+] Getting handshake .\n");
+    read(socketClient,value,sizeof(value)); // read(socket,var,size of var)
+    printf("[+] The value to config the pages is %s\n",value);
     printf("-------------------------\n");
     sleep(2);
 
@@ -128,6 +166,24 @@ void connectToFL(char *IP,char *PORT){
 }
 
 
+
+void checkPackage(void *package,int packageNumber){
+
+  switch(packageNumber){
+    case 1:
+      printf("SELECT\n" );
+      //its select , we have to :
+
+      // Verifica si existe el segmento de la tabla solicitada y busca en las páginas del
+      //mismo si contiene key solicitada. Si la contiene, devuelve su valor y finaliza el proceso.
+
+      //if(package.key.segment = exist) return key;
+      //else askToFS(key); //Si no la contiene, envía la solicitud a
+                    //FileSystem para obtener el valor solicitado y almacenarlo.
+                //Una vez obtenido se debe solicitar una nueva página libre para almacenar el mismo. [sigue..]
+  }
+
+}
 
 void leerMensaje(void *recibido,header request){
 
@@ -144,29 +200,80 @@ void leerMensaje(void *recibido,header request){
                 enviarAFileSystem(INSERT,request.sizeData,recibido);
             }
             case SELECT:{
-                structSelect * instruccionSelect = desserealizarInsert(recibido);
-                //verificar si esta en cache
+                st_select * instruccionSelect = deserealizarSelect(recibido); //desserealizarSelect
+                //check
+                checkPackage(recibido,1);// the number for SELECT is 1 (one)  ( we sould make a dictionary)
                 enviarAFileSystem(SELECT,request.sizeData,recibido);
             }
 
+            //we can make a error handler func, in where we put the instruction value & the func decides how to hand it
+            // if we dont do this, we have to do that in each instruction
 
-            // -------------------------------------------- //
-            //----------------------------------------------//
+
+
+
+
+
 
             //the next cases arent necessary 4 tomorrow mates ;) ;) ;)
 
             case CREATE:{
-                structCreate * instruccionCreate = desserealizarInsert(recibido);
+                structCreate * instruccionCreate = desserealizarInsert(recibido); //desserealizarCreate
                 //..
                 enviarAFileSystem(CREATE,request.sizeData,recibido);
             }
             case DROP:{
-                structDrop * intruccionDrop = desserealizarInsert(recibido);
+                structDrop * intruccionDrop = desserealizarInsert(recibido); //desserealizarDrop
                 //..
                 enviarAFileSystem(DROP,request.sizeData,recibido);
             }
         }
     }
+}
+
+
+
+void enviarMjsString(int socket, t_log * log){
+    // creamos el header
+    char * bloqueData = "Hello hello";
+    int control = 0;
+    header headSend;
+    //para identificar de que componente viene
+    headSend.letra = 'M';
+    //para identificar el tipo de mensaje
+    headSend.codigo = 1;
+    //tamaño de la data que se va enviar en este caso es un string usamos strlen
+    //y le sumamos 1 por que hay q agregarle el \0 el fin de cadena
+    headSend.sizeData = strlen(bloqueData) + 1;
+    //creamos el mjs y nos devuelve el paquetes de datos a enviar pasamos la direccion del header y el bloque de data a enviar
+    message *bufferRes = createMessage(&headSend, bloqueData);
+    //enviamos el mjs se pasa el socket , la direccion del log para logear error interno y un direccion de int para tomar accion
+    //sobre los resultados
+    if (enviar_message(socket, bufferRes, log, &control) < 0) {
+        log_info(log, "[-] Failed to send the block :( \n");
+    }
+    log_info(log, "[+] Message sended ;) \n");
+}
+
+
+void enviarAFileSystem(enum OPERACION operacion,size_t sizeBuffer,void * buffer){
+    printf("[+] Sending to FS ..");
+
+    t_log *file_log = crear_archivo_log("PrimaryMemory", true,"./logC");
+    int control = 0;
+
+    printf("[+]Connecting to FILESYSTEM AT : %s:%s \n",IP_FS,PORT_FS);
+
+    int socketClient = establecerConexion(IP_FS,PORT_FS,file_log,&control);
+
+    if (control != 0) {
+        log_error(file_log, "[-] Failed to connect. \n");
+        return -1;
+    }
+
+    //make package & send it
+
+    //send(socketClient,package,..,..);
 }
 
 
@@ -245,33 +352,4 @@ void start_seeding(char *IP,char *PORT){
   enviarMjsString(socketClient,file_log);
   printf("-------------------------\n");
   sleep(2);
-}
-
-
-
-void enviarMjsString(int socket, t_log * log){
-    // creamos el header
-    char * bloqueData = "hola Como va";
-    int control = 0;
-    header headSend;
-    //para identificar de que componente viene
-    headSend.letra = 'M';
-    //para identificar el tipo de mensaje
-    headSend.codigo = 1;
-    //tamaño de la data que se va enviar en este caso es un string usamos strlen
-    //y le sumamos 1 por que hay q agregarle el \0 el fin de cadena
-    headSend.sizeData = strlen(bloqueData) + 1;
-    //creamos el mjs y nos devuelve el paquetes de datos a enviar pasamos la direccion del header y el bloque de data a enviar
-    message *bufferRes = createMessage(&headSend, bloqueData);
-    //enviamos el mjs se pasa el socket , la direccion del log para logear error interno y un direccion de int para tomar accion
-    //sobre los resultados
-    if (enviar_message(socket, bufferRes, log, &control) < 0) {
-        log_info(log, "Error al enviar el bloque");
-    }
-    log_info(log, "Se envio el mjs vaya a mirar la respuesta en el server");
-}
-
-
-void enviarAFileSystem(enum OPERACION operacion,size_t sizeBuffer,void * buffer){
-    printf("[+] Sending to FL");
 }
